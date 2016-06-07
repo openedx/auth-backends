8684513ac99f87ed2c3a90cfe1782d6eed0d5667
Clinton Blackburn <cblackburn@edx.org>
2016-06-07 11:36:23 -0400

    Added login view and URL patterns
    We are duplicating this code across our repositories. This codifies those duplications into a single location.
    
    ECOM-4607

--- auth_backends/tests/test_views.py
+++ auth_backends/tests/test_views.py
@@ -1,14 +1,16 @@
 """ Tests for the views module. """
 
 from django.conf.urls import url
+from django.core.urlresolvers import reverse
 from django.test import TestCase, override_settings
 
 from auth_backends.tests.mixins import LogoutViewTestMixin
+from auth_backends.urls import auth_urlpatterns
 from auth_backends.views import LogoutRedirectBaseView
 
 LOGOUT_REDIRECT_URL = 'https://www.example.com/logout/'
 
-urlpatterns = [
+urlpatterns = auth_urlpatterns + [
     url(r'^logout/$', LogoutRedirectBaseView.as_view(url=LOGOUT_REDIRECT_URL), name='logout'),
 ]
 
@@ -19,3 +21,13 @@ class LogoutRedirectBaseViewTests(LogoutViewTestMixin, TestCase):
 
     def get_redirect_url(self):
         return LOGOUT_REDIRECT_URL
+
+
+@override_settings(ROOT_URLCONF=__name__)
+class EdxOpenIdConnectLoginViewTests(TestCase):
+    """ Tests for EdxOpenIdConnectLoginView. """
+
+    def test_redirect(self):
+        """ Verify the view redirects to the edX OIDC login page. """
+        response = self.client.get(reverse('login'))
+        self.assertRedirects(response, reverse('social:begin', args=['edx-oidc']), fetch_redirect_response=False)
---
+++ auth_backends/urls.py
@@ -0,0 +1,12 @@
+""" Shared URL patterns.
+
+Add these to your project's `urlpatterns` to avoid duplicating code.
+"""
+from django.conf.urls import url, include
+
+from auth_backends.views import EdxOpenIdConnectLoginView
+
+auth_urlpatterns = [  # pylint: disable=invalid-name
+    url(r'^login/$', EdxOpenIdConnectLoginView.as_view(), name='login'),
+    url('', include('social.apps.django_app.urls', namespace='social')),
+]
