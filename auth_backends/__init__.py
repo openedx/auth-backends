f15e4aaa45fdcc480c83716cbe05351271959d9b
Clinton Blackburn <cblackburn@edx.org>
2016-06-06 21:08:28 -0400

    Added base logout view
    ECOM-4607

--- auth_backends/__init__.py
+++ auth_backends/__init__.py
@@ -3,4 +3,4 @@
  These package is designed to be used primarily with Open edX Django projects, but should be compatible with non-edX
  projects as well.
 """
-__version__ = '0.2.3'  # pragma: no cover
+__version__ = '0.3.0'  # pragma: no cover
