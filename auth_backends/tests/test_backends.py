5a8c5e0e44a579065b7b081e09f1ab9c1ce9c957
Clinton Blackburn <clinton.blackburn@gmail.com>
2017-04-21 23:48:26 -0400

    Added OAuth 2.0 backend
    This backend will eventually replace the OpenID Connect backend.
    
    LEARNER-724

--- auth_backends/tests/test_backends.py
+++ auth_backends/tests/test_backends.py
@@ -8,12 +8,18 @@ import ddt
 import mock
 import pytest
 import six
-from auth_backends.strategies import EdxDjangoStrategy
-from jwkest.jwk import SYMKey
+from Cryptodome.PublicKey import RSA
+from django.core.cache import cache
+from httpretty import HTTPretty
+from jwkest.jwk import SYMKey, RSAKey, KEYS
 from jwkest.jws import JWS
 from jwkest.jwt import b64encode_item
+from social_core.exceptions import AuthTokenError
 from social_core.tests.backends.oauth import OAuth2Test
 from social_core.tests.backends.open_id_connect import OpenIdConnectTestMixin
+
+from auth_backends.backends import EdXOpenIdConnect
+from auth_backends.strategies import EdxDjangoStrategy
 
 
 @ddt.ddt
@@ -42,7 +48,7 @@ class EdXOpenIdConnectTests(OpenIdConnectTestMixin, OAuth2Test):
     # NOTE (CCB): We are TEMPORARILY disabling the nonce validation while we transition our
     # authentication provider to properly implement storing the nonce at the point of initial
     # authorization, rather than when we request the access token.
-    def access_token_body(self, request, _url, headers):    # pylint: disable=method-hidden
+    def access_token_body(self, request, _url, headers):  # pylint: disable=method-hidden
         """
         Get the nonce from the request parameters, add it to the id_token, and
         return the complete response.
@@ -159,3 +165,185 @@ class EdXOpenIdConnectTests(OpenIdConnectTestMixin, OAuth2Test):
         # class being used does not rely on Django settings.
         self.strategy.set_settings({'SOCIAL_AUTH_EDX_OIDC_PUBLIC_URL_ROOT': expected})
         self.assertEqual(self.backend.AUTHORIZATION_URL, expected + authorize_path)
+
+    def test_deprecated(self):
+        """ Attempts to instantiate EdXOpenIdConnect should fire a warning. """
+        with mock.patch('warnings.warn') as mock_warn:
+            EdXOpenIdConnect(self.strategy, redirect_uri=self.complete_url)
+            mock_warn.assert_called_once_with(
+                'EdXOpenIdConnect is deprecated. Please use EdXOAuth2.', DeprecationWarning
+            )
+
+
+class EdXOAuth2Tests(OAuth2Test):
+    """ Tests for the EdXOAuth2 backend. """
+
+    backend_path = 'auth_backends.backends.EdXOAuth2'
+    client_key = 'a-key'
+    client_secret = 'a-secret-key'
+    expected_username = 'jsmith'
+    hmac_key = SYMKey(key='fake-key')
+    openid_config_body = {
+        'access_token_signing_alg_values_supported': [
+            'RS512',
+            'HS256'
+        ],
+        'claims_supported': [
+            'sub',
+            'iss',
+            'name',
+            'given_name',
+            'family_name',
+            'email'
+        ],
+        'token_endpoint': 'https://example.com/oauth2/access_token',
+        'token_endpoint_auth_methods_supported': [
+            'client_secret_post'
+        ],
+        'jwks_uri': 'https://example.com/oauth2/jwks.json',
+        'end_session_endpoint': 'https://example.com/logout',
+        'authorization_endpoint': 'https://example.com/oauth2/authorize',
+        'scopes_supported': [
+            'openid',
+            'profile',
+            'email'
+        ],
+        'issuer': 'https://example.com/oauth2'
+    }
+    provider_endpoint = 'https://example.com/oauth2'
+
+    def setUp(self):
+        cache.clear()
+        super(EdXOAuth2Tests, self).setUp()
+        self.key = RSAKey(kid='testkey', key=RSA.generate(2048))
+
+        HTTPretty.register_uri(
+            HTTPretty.GET,
+            self.provider_endpoint + '/.well-known/openid-configuration',
+            status=200,
+            body=json.dumps(self.openid_config_body)
+        )
+
+        provider_keys = KEYS()
+        provider_keys.add(self.key.serialize())
+
+        HTTPretty.register_uri(
+            HTTPretty.GET,
+            self.openid_config_body['jwks_uri'],
+            status=200,
+            body=provider_keys.dump_jwks()
+        )
+
+    def access_token_body(self, request, _url, headers):  # pylint: disable=method-hidden,unused-argument
+        """ Generates a response from the provider's access token endpoint. """
+        # The backend should always request JWT access tokens, not Bearer.
+        body = six.moves.urllib.parse.parse_qs(request.body.decode('utf8'))
+        self.assertEqual(body['token_type'], ['jwt'])
+
+        expires_in = 3600
+        access_token = self.create_jws_access_token(expires_in)
+        body = json.dumps({
+            'scope': 'read write profile email',
+            'token_type': 'JWT',
+            'expires_in': expires_in,
+            'access_token': access_token
+        })
+        return 200, headers, body
+
+    def create_jws_access_token(self, expires_in=3600, issuer=None, key=None, alg='RS512'):
+        """
+        Creates a signed (JWS) access token.
+
+        Arguments:
+            expires_in (int): Number of seconds after which the token expires.
+            issuer (str): Issuer of the token.
+            key (jwkest.jwk.Key): Key used to sign the token.
+            alg (str): Signing algorithm.
+
+        Returns:
+            str: JWS
+        """
+        key = key or self.key
+        now = datetime.datetime.utcnow()
+        expiration_datetime = now + datetime.timedelta(seconds=expires_in)
+        issue_datetime = now
+        issuer = issuer or self.openid_config_body['issuer']
+        payload = {
+            'iss': issuer,
+            'administrator': False,
+            'iat': timegm(issue_datetime.utctimetuple()),
+            'given_name': 'Joe',
+            'sub': 'e3bfe0e4e7c6693efba9c3a93ee7f31b',
+            'preferred_username': self.expected_username,
+            'aud': 'InkocujLikyucsEdwiWatdebrEackmevLakDuifKooshkakWow',
+            'scopes': ['read', 'write', 'profile', 'email'],
+            'email': 'jsmith@example.com',
+            'exp': timegm(expiration_datetime.utctimetuple()),
+            'name': 'Joe Smith',
+            'family_name': 'Smith'
+        }
+        access_token = JWS(payload, jwk=key, alg=alg).sign_compact()
+        return access_token
+
+    def extra_settings(self):
+        settings = super(EdXOAuth2Tests, self).extra_settings()
+        settings.update({
+            'SOCIAL_AUTH_{0}_KEY'.format(self.name): self.client_key,
+            'SOCIAL_AUTH_{0}_SECRET'.format(self.name): self.client_secret,
+            'SOCIAL_AUTH_{0}_ENDPOINT'.format(self.name): self.provider_endpoint,
+            'SOCIAL_AUTH_{0}_JWS_HMAC_SIGNING_KEY'.format(self.name): self.hmac_key.key,
+
+        })
+        return settings
+
+    def test_login(self):
+        self.do_login()
+
+    def test_partial_pipeline(self):
+        self.do_partial_pipeline()
+
+    def test_end_session_url(self):
+        """ The method should return the provider's logout URL. """
+        self.assertEqual(self.backend.end_session_url(), self.openid_config_body['end_session_endpoint'])
+
+    def test_invalid_issuer(self):
+        """ AuthTokenError should be raised if the access token's issuer is incorrect. """
+        access_token = self.create_jws_access_token(issuer='other-issuer')
+
+        expected_msg = 'Token error: Invalid issuer. Expected {}. Received other-issuer.'.format(
+            self.openid_config_body['issuer']
+        )
+        with self.assertRaisesRegex(AuthTokenError, expected_msg):
+            self.backend.user_data(access_token)
+
+    def test_expired_access_token(self):
+        """ AuthTokenError should be raised if the issued access token is expired. """
+        access_token = self.create_jws_access_token(expires_in=-3600)
+        with self.assertRaisesRegex(AuthTokenError, 'Access token has expired'):
+            self.backend.user_data(access_token)
+
+    def test_hmac_signing_key(self):
+        """ The backend should support HMAC signing keys in addition to asymmetric. """
+        access_token = self.create_jws_access_token(key=self.hmac_key, alg='HS512')
+        self.backend.user_data(access_token)
+
+    def test_provider_configuration_caching(self):
+        """ The provider configuration data should be cached by default. """
+        cache.clear()
+        self.assertEqual(self.backend.provider_configuration(), self.openid_config_body)
+
+        HTTPretty.disable()
+        self.assertEqual(self.backend.provider_configuration(), self.openid_config_body)
+
+    def test_get_jwks_keys_caching(self):
+        """ The JWKS keys should be cached by default. """
+        expected = KEYS()
+        expected.add(self.key.serialize())
+        expected.add(self.hmac_key.serialize())
+
+        # NOTE: KEYS objects have no __eq__ method defined, so we compare their string representations,
+        # which are JWK sets.
+        self.assertEqual(str(self.backend.get_jwks_keys()), str(expected))
+
+        HTTPretty.disable()
+        self.assertEqual(str(self.backend.get_jwks_keys()), str(expected))
