22a5985cfd29b87a1215a6a38d5ab07ab19e7508
Awais Jibran <awaisdar001@gmail.com>
2019-05-13 14:02:18 +0500

    Create new Version for auth-backends for release

--- auth_backends/__init__.py
+++ auth_backends/__init__.py
@@ -3,4 +3,4 @@
  These package is designed to be used primarily with Open edX Django projects, but should be compatible with non-edX
  projects as well.
 """
-__version__ = '2.0.0'  # pragma: no cover
+__version__ = '2.0.1'  # pragma: no cover
