afe3979fcfa27ed00491f2dbfd8f1fe8a6bdfe97
Aarif <mraarif@outlook.com>
2021-02-02 22:25:11 +0500

    fixed the change log and updated the version

--- auth_backends/__init__.py
+++ auth_backends/__init__.py
@@ -3,4 +3,4 @@
  These package is designed to be used primarily with Open edX Django projects, but should be compatible with non-edX
  projects as well.
 """
-__version__ = '3.3.2'  # pragma: no cover
+__version__ = '3.3.3'  # pragma: no cover
