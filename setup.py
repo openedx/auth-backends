8738cb413be991eaf96e22bf016207fde0685961
Usama Sadiq <usama7274@gmail.com>
2025-04-09 15:53:53 +0500

    feat: add django 5.2 support

--- setup.py
+++ setup.py
@@ -134,6 +134,7 @@ setup(
         'Programming Language :: Python :: 3.12',
         'Framework :: Django',
         'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.2',
         'Topic :: Internet',
     ],
     keywords='authentication edx',
